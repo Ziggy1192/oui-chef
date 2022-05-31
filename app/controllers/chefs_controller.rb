fb675a09a8550ee500d102f3883e4c3640b2a4df
Dziugas Bartkevicius <bartkevicius.dziugas@gmail.com>
2022-05-31 12:03:12 +0100

    routes kinda OK to go for now

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,2 +1,24 @@
 class ChefsController < ApplicationController
+  def index
+    @chefs = Chef.all
+  end
+
+  def show
+    @chef = Chef.find(params[:id])
+  end
+
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    @chef.save
+  end
+
+  private
+
+  def chef_params
+    params.require(:chef).permit(:cuisine, :speciality, :profile)
+  end
 end
